1093b7b8c6430e0ee002b7c8147fee69c2cd3718
CareyLiu <15946250162@163.com>
2020-09-16 11:59:26 +0800

    适配

--- app/src/main/java/com/smarthome/magic/activity/FengNuanActivity.java
+++ app/src/main/java/com/smarthome/magic/activity/FengNuanActivity.java
@@ -3,6 +3,7 @@ package com.smarthome.magic.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
@@ -30,5 +31,26 @@ public class FengNuanActivity extends BaseActivity {
         Intent intent = new Intent(context, FengNuanActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
+    }
+
+    @Override
+    protected void initToolbar() {
+        super.initToolbar();
+        tv_title.setText("风暖加热器");
+        tv_title.setTextSize(17);
+        tv_title.setTextColor(getResources().getColor(R.color.black));
+        mToolbar.setNavigationIcon(R.mipmap.backbutton);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //imm.hideSoftInputFromWindow(findViewById(R.id.cl_layout).getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean showToolBar() {
+        return true;
     }
 }

aaeebeb276be4ee78231fbe39a604cf2a2ffb9b6
yanxianghe1992 <yanxianghe1992@qq.com>
2020-11-04 14:58:51 +0800

    设备列表，共享设备优化   水暖加热器优化 空调  新增定时页，空调状态页。修改故障弹窗 修配厂页面

---
+++ app/src/main/java/com/smarthome/magic/activity/xiupeichang/adapter/XiupeichangPingjiaAdapter.java
@@ -0,0 +1,19 @@
+package com.smarthome.magic.activity.xiupeichang.adapter;
+
+import com.smarthome.magic.baseadapter.baserecyclerviewadapterhelper.BaseQuickAdapter;
+import com.smarthome.magic.baseadapter.baserecyclerviewadapterhelper.BaseViewHolder;
+
+import java.util.List;
+
+import androidx.annotation.Nullable;
+
+public class XiupeichangPingjiaAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+    public XiupeichangPingjiaAdapter(int layoutResId, @Nullable List<String> data) {
+        super(layoutResId, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, String item) {
+
+    }
+}
